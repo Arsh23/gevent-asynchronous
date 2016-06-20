b89bdef03920fbc4b175ab904c369d94cbc9fb6b
Akul Mehra <mehra.akul@gmail.com>
2016-06-21 02:48:46 +0530

    initial queue code

---
+++ crawler_gevent_3.py
@@ -0,0 +1,45 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+# import gevent's monkey patch
+
+import gevent.pool
+import gevent.queue
+
+import sys
+import re
+import requests
+from  datetime import datetime
+
+crawled = 0
+startTime = datetime.now()
+pool = gevent.pool.Pool(5)
+queue = gevent.queue.Queue()
+
+def crawler():
+    global crawled
+
+    while 1:
+        try:
+            u = queue.get(timeout=1)
+            response = requests.get(u)
+            print response.status_code, u
+
+            for link in re.findall('<a href="(http.*?)"', response.content):
+
+                if crawled < 10:
+                    crawled += 1
+                    queue.put(link)
+
+        except gevent.queue.Empty:
+            break
+
+# Read the seed url from stdin
+queue.put(sys.argv[1])
+
+for x in xrange(0, 5):
+    pool.spawn(crawler)
+
+# Wait for everything to complete
+pool.join()
+
+print datetime.now() - startTime # Took 6.943 seconds, varying
